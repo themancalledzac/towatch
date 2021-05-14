1e844fa007146b1c90790f27a1ec932959029846
Zac Edens <themancalledzac@gmail.com>
2021-05-14 12:06:21 -0700

    initial backend now functional with keystone. next up is user auth

---
+++ backEnd/keystone.ts
@@ -0,0 +1,33 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+import 'dotenv/config';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/towatch';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+    User,
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: add session values here
+});
